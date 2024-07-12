fb18d694ff96938985825b6218b44aefcacd3992
GrimmTMM <themagnetboy1207@gmail.com>
2024-07-13 01:13:10 +0700

    Encased everything in functions

--- src/termchatter/setup.py
+++ src/termchatter/setup.py
@@ -1,40 +1,42 @@
 import sys
 
-id_config = './.appid'
-key_config = './.appkey'
-secret_config = './.secret'
-cluster_config = './.cluster'
-channel_name = './.channel'
-user_name = './.username'
-channel = ''
-username = ''
-if(not len(sys.argv) <= 1):
-    if(sys.argv[1] == 'pusher'):
-        if(len(sys.argv) == 6):
-            app_id = sys.argv[2]
-            app_key = sys.argv[3]
-            secret = sys.argv[4]
-            cluster = sys.argv[5]
-            file = open(id_config, 'w')
-            file.write(app_id)
-            file = open(key_config, 'w')
-            file.write(app_key)
-            file = open(secret_config, 'w')
-            file.write(secret)
-            file = open(cluster_config, 'w')
-            file.write(cluster)
+def main(args):
+    print(args)
+    id_config = './.appid'
+    key_config = './.appkey'
+    secret_config = './.secret'
+    cluster_config = './.cluster'
+    channel_name = './.channel'
+    user_name = './.username'
+    channel = ''
+    username = ''
+    if(not len(args) <= 0):
+        if(args[0] == 'pusher'):
+            if(len(args) == 5):
+                app_id = args[1]
+                app_key = args[2]
+                secret = args[3]
+                cluster = args[4]
+                file = open(id_config, 'w')
+                file.write(app_id)
+                file = open(key_config, 'w')
+                file.write(app_key)
+                file = open(secret_config, 'w')
+                file.write(secret)
+                file = open(cluster_config, 'w')
+                file.write(cluster)
+                exit()
+        elif(len(args) == 1):
+            channel = args[0]
+            file = open(channel_name, 'w')
+            file.write(channel)
             exit()
-    elif(len(sys.argv) == 2):
-        channel = sys.argv[1]
-        file = open(channel_name, 'w')
-        file.write(channel)
-        exit()
-    elif(len(sys.argv) == 3):
-        channel = sys.argv[1]
-        username = sys.argv[2]
-        file = open(channel_name, 'w')
-        file.write(channel)
-        file = open(user_name, 'w')
-        file.write(username)
-        exit()
-print("Type 'chat help' for guidance")
+        elif(len(args) == 2):
+            channel = args[0]
+            username = args[1]
+            file = open(channel_name, 'w')
+            file.write(channel)
+            file = open(user_name, 'w')
+            file.write(username)
+            exit()
+    print("Type 'chat help' for guidance")
